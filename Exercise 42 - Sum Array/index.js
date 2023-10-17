bb4c0d69936872899a501aa59209445bc9a6fd95
Fr3nk21 <104112709+Fr3nk21@users.noreply.github.com>
2023-10-18 06:22:14 +1100

    Exercise 42 and 43

--- Exercise 42 - Sum Array/index.js
+++ Exercise 42 - Sum Array/index.js
@@ -1,4 +1,15 @@
 // DEFINE YOUR FUNCTION BELOW:
-function sumArray(arr) {
+function sumArray(nums) {
+
+  let total = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    total += nums[i];
+  }
+  return total;
 
 }
+
+sumArray([1,2,3]);
+sumArray([2,2,2,2]);
+sumArray([50,50,1]);
