aee40a749c06d188abb721f28319188ff4ffde3e
Fr3nk21 <104112709+Fr3nk21@users.noreply.github.com>
2023-11-27 07:52:48 +1100

    Exercise 54

---
+++ Exercise 54 - Manipulating Attributes/app.js
@@ -0,0 +1,4 @@
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+const eggImg = document.querySelector('img');
+eggImg.src = 'https://devsprouthosting.com/images/chicken.jpg';
+eggImg.alt = 'chicken';
