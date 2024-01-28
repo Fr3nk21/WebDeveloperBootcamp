a4dc06c6e0049ba4a5f8acc01df2981b678690d2
Fr3nk21 <104112709+Fr3nk21@users.noreply.github.com>
2024-01-29 07:16:41 +1100

    Exercise 61

---
+++ Exercise 61 - Form Events/app.js
@@ -0,0 +1,14 @@
+// Leave the next line, the form must be assigned to a variable named 'form' in order for the exercise test to pass
+const form = document.querySelector('form');
+
+const listProducts = document.querySelector('#list');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const qty = form.elements.qty.value;
+    const product = form.elements.product.value;
+    const li = document.createElement('li');
+    li.innerText = `${qty} ${product}`;
+    listProducts.appendChild(li);
+    form.reset();
+});
